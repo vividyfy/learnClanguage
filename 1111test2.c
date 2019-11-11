0721fbf19db07511427675236797266b1a827c44
vividyfy <1044292734@qq.com>
2019-11-11 23:29:47 +0800

    判断1000年---2000年之间的闰年

---
+++ 1111test2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int i = 1000;
+	for (i = 1000; i < 2001; i++){
+		if (i % 4 == 0)
+		if (i % 100!=0)
+		{
+			printf("%d\n", i); }
+		if (i % 400 == 0) {
+			printf("%d\n", i);
+		}
+	}			
+system("pause");
+}	
+
+
+	
+	
